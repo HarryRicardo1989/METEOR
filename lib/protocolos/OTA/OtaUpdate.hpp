f2b68a715e541e87fd514f82cc3b8963f04096c0
HarryRicardo1989 <ricardo.paschoali@gmail.com>
2024-03-19 12:10:27 -0300

    ok

--- lib/protocolos/OTA/OtaUpdate.hpp
+++ lib/protocolos/OTA/OtaUpdate.hpp
@@ -11,5 +11,5 @@ public:
     esp_err_t start(const char *ota_url);
 
 private:
-    static esp_err_t http_event_handler(esp_http_client_event_t *evt); //
+    static esp_err_t http_event_handler(esp_http_client_event_t *evt); // Corrigido
 };
